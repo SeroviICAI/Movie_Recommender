a08b8bb0e3183de2c9063628f05b267f1edc7201
SeroviICAI <91453546+SeroviICAI@users.noreply.github.com>
2022-11-17 12:55:17 +0100

    Delete movie_recommender directory

--- movie_recommender/src/constants.py
+++
@@ -1,7 +0,0 @@
-SAVE_PATH = r'../data/'
-
-GENRES = ['action', 'adventure', 'animation', 'biography', 'comedy', 'crime', 'documentary', 'drama',
-          'family', 'fantasy', 'film-noir', 'history', 'horror', 'music', 'musical', 'mystery',
-          'romance', 'sci-fi', 'short', 'sport', 'thriller', 'war', 'western']
-
-IMDB_URL = 'https://www.imdb.com/search/title/?genres={genre}&start={start}&explore=title_type,genres&ref_=adv_nxt'
